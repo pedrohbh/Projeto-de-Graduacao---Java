b0b1829162c6469f82ac5b0d17b38907c6c6cb93
Eu <pedrohbh@users.noreply.github.com>
2017-05-03 16:03:53 -0300

    Inicio da elaboração de testes do algoritmo A*

---
+++ src/pedro/pg/testescomputacionais/TesteAEstrela.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pedro.pg.testescomputacionais;
+
+import java.util.Formatter;
+
+/**
+ *
+ * @author administrador
+ */
+public class TesteAEstrela 
+{
+    private static Formatter output;
+    private static final int NUM_RODADAS = 5;
+    
+}
