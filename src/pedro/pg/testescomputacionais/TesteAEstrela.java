836e6745783ed2de744aa49c595703f2dfd26191
Eu <pedrohbh@users.noreply.github.com>
2017-05-03 16:12:14 -0300

    Inicio da elaboração de testes do algoritmo A* - parte 2

--- src/pedro/pg/testescomputacionais/TesteAEstrela.java
+++ src/pedro/pg/testescomputacionais/TesteAEstrela.java
@@ -5,6 +5,7 @@
  */
 package pedro.pg.testescomputacionais;
 
+import java.io.FileNotFoundException;
 import java.util.Formatter;
 
 /**
@@ -15,5 +16,24 @@ public class TesteAEstrela
 {
     private static Formatter output;
     private static final int NUM_RODADAS = 5;
+    
+    public static void openFile()
+    {
+        try
+        {
+            output = new Formatter("ResultadosAEstrela.csv");
+            output.format( "Nome Instância;Número de Vértices;Número de Arestas;Tempo médio Dijkstra;Tempo médio Dijkstra Adptado;Tempo médio A*;Tempo médio A* Manhattan%n");
+        }
+        catch ( FileNotFoundException e )
+        {
+            System.err.println("Erro ao abrir o arquivo de escrita \"ResultadosAEstrela.csv\".");
+            System.exit( 1 );
+        }
+        catch ( SecurityException e )
+        {
+            System.err.println("Error de permissão de escrita no arquivo \"ResultadosAEstrela.csv\".");
+            System.exit( 1 );
+        }
+    }
     
 }
